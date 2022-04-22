b7b4729d859c5772d58e699e4a7e9dcfbfdafdea
Soledad Spoto <sole.spoto@gmail.com>
2022-04-22 18:46:44 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//Aquí arrancará el servidor - REST API
+
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { mongoose } = require('./database');
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares - funciones que se ejecutan antes de que lleguen a las rutas
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks/', require('./routes/task.routes'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Starting the server
+app.listen (app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
